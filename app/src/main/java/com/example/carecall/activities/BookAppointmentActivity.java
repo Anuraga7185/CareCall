50c108b1a6929dccf3cd20ff9146b39271bbebdf
Anuraga7185 <ranurag378@gmail.com>
2024-11-29 05:33:17 +0530

    Changes first time on this Branch with Activity name Changed

--- app/src/main/java/com/example/carecall/activities/BookAppointmentActivity.java
+++ app/src/main/java/com/example/carecall/activities/BookAppointmentActivity.java
@@ -2,18 +2,12 @@ package com.example.carecall.activities;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.example.carecall.R;
 import com.example.carecall.databinding.ActivityBookAppointmentBinding;
-import com.example.carecall.databinding.ActivityDetailBinding;
 import com.example.carecall.entity.DoctorData;
 
-public class BookAPpointmentActivity extends AppCompatActivity {
+public class BookAppointmentActivity extends AppCompatActivity {
     ActivityBookAppointmentBinding binding;
 
     @Override
